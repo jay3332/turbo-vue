24690fd3097071c333007a9e084d53ddb438d5fc
jay3332 <40323796+jay3332@users.noreply.github.com>
2024-04-08 23:31:50 -0400

    add firewall tip in homescreen

--- src/pages/Grades.tsx
+++ src/pages/Grades.tsx
@@ -51,30 +51,36 @@ export function GradesInner(props: { gradingPeriod: string }) {
   const api = getApi()!
 
   return (
-    <div class="flex flex-col p-2">
-      <div class="flex gap-2 flex-wrap">
-        <For each={api.courseOrders.get(props.gradingPeriod)}>
-          {(course: CourseMetadata) => (
-            <A
-              href={`/grades/${props.gradingPeriod}/${course.ID}`}
-              class="flex justify-between items-center p-4 rounded-lg w-full lg:w-[calc(50%-0.25rem)] bg-bg-0/50
-                transition hover:bg-bg-3/80"
-            >
-              <div class="flex flex-col">
-                <span class="font-title font-medium md:text-lg">{course.Name}</span>
-                <span class="flex items-center gap-x-3 mobile-xs:flex-col mobile-xs:items-start">
-                  <span class="text-sm text-fg/50 text-left">{course.TeacherName}</span>
-                  <span class="flex items-center gap-x-1 text-left">
-                    <Icon icon={LocationDot} class="w-4 h-4 fill-fg/50" />
-                    <span class="text-sm text-fg/50">{course.room}</span>
+    <div class="flex flex-col h-full justify-between">
+      <div class="flex flex-col p-2">
+        <div class="flex gap-2 flex-wrap">
+          <For each={api.courseOrders.get(props.gradingPeriod)}>
+            {(course: CourseMetadata) => (
+              <A
+                href={`/grades/${props.gradingPeriod}/${course.ID}`}
+                class="flex justify-between items-center p-4 rounded-lg w-full lg:w-[calc(50%-0.25rem)] bg-bg-0/50
+                  transition hover:bg-bg-3/80"
+              >
+                <div class="flex flex-col">
+                  <span class="font-title font-medium md:text-lg">{course.Name}</span>
+                  <span class="flex items-center gap-x-3 mobile-xs:flex-col mobile-xs:items-start">
+                    <span class="text-sm text-fg/50 text-left">{course.TeacherName}</span>
+                    <span class="flex items-center gap-x-1 text-left">
+                      <Icon icon={LocationDot} class="w-4 h-4 fill-fg/50" />
+                      <span class="text-sm text-fg/50">{course.room}</span>
+                    </span>
                   </span>
-                </span>
-              </div>
-              <CourseGrade gradingPeriod={props.gradingPeriod} course={course} />
-            </A>
-          )}
-        </For>
+                </div>
+                <CourseGrade gradingPeriod={props.gradingPeriod} course={course} />
+              </A>
+            )}
+          </For>
+        </div>
       </div>
+      <p class="px-4 pt-2 pb-4 text-center text-xs text-fg/50">
+        Tip: You can access a firewalled version of TurboVUE on your school Chromebook at&nbsp;
+        <a href="https://turbo-vue.github.io" class="font-bold">https://turbo-vue.github.io</a>
+      </p>
     </div>
   )
 }
